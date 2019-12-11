8edf823293543bd2df5b80a5ab3edda430d709a1
beerar <beerar@HYDVMIOBEERAR1.corp.amvescap.net>
2019-12-11 19:12:21 +0530

    this is java practises prog

---
+++ src/practice/method.java
@@ -0,0 +1,34 @@
+package practice;
+
+public class method {
+
+	 public static int square() {
+		 int result;
+		result = 10*10;
+	 
+
+	        // return statement
+	      return result;
+	 }
+	 
+	 public static String Raj(){
+		
+		 String r = "teja";
+		// System.out.println("relt");
+	 return r;
+		
+	 }
+	public static void myMethod(){
+		System.out.println("My first Java");
+	}
+	
+public static void main(String[] args){
+	System.out.println("testing methods");
+	myMethod();
+	Raj();
+	square();
+	//System.out.println(square(10,12));
+	
+	}
+
+}
